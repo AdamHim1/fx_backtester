07a4b81f20275bd4edd6cc44c16971dd5e0c6e02
AdamHim1 <adam.him@dauphine.eu>
2024-12-29 18:50:04 +0100

    Initial project structure generated by cookiecutter

---
+++ docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use FX Strategies Backtester in a project::
+
+    import fx_backtester
---
+++ tests/test_fx_backtester.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `fx_backtester` package."""
+
+
+import unittest
+
+from fx_backtester import fx_backtester
+
+
+class TestFx_backtester(unittest.TestCase):
+    """Tests for `fx_backtester` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
